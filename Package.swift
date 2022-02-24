a3382ae6748da23718b590e69b0f40e35d5cb48e
狄烨 <zhangjinhu>
2022-02-24 15:26:03 +0800

    release

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.0.0"),
-        .package(url: "https://github.com/jackiehu/SwiftButton.git", from: "1.0.0"),
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.0")),
+        .package(url: "https://github.com/jackiehu/SwiftButton.git", .upToNextMajor(from: "1.0.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- SwiftShow.podspec
+++ SwiftShow.podspec
@@ -1,7 +1,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'SwiftShow'
-  s.version          = '0.7.0'
+  s.version          = '0.7.1'
   s.summary          = '弹窗组件.'
  
   s.description      = <<-DESC
